0813f7d6ef4461caa19413fc94c1019463158dd2
Abhinandan <abhinandan19909@gmail.com>
2025-11-19 11:55:30 +0400

    Allow QR code images from api.qrserver.com

--- server/src/index.ts
+++ server/src/index.ts
@@ -14,8 +14,14 @@ import { errorHandler } from "./middleware/errorHandler";
 
 const app = express();
 
-app.use(helmet());
-app.use(morgan("dev"));
+app.use(helmet({
+  contentSecurityPolicy: {
+    directives: {
+      ...helmet.contentSecurityPolicy.getDefaultDirectives(),
+      "img-src": ["'self'", "data:", "https://api.qrserver.com"],
+    },
+  },
+}));app.use(morgan("dev"));
 app.use(express.json({ limit: "1mb" }));
 
 app.use(
